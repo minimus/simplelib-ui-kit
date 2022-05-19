e97fcc9bcae949500fa853b6f0cedac47e82ddaa
konstantin <rublezoid@gmail.com>
2022-05-19 17:35:07 +0300

    fixed: publish features

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import ProgressFab from './buttons/ProgressFab/ProgressFab'
-import TextInput from './inputs/TextInput'
-import ColorsInput from './inputs/ColorsInput'
-import CrossTextInput from './inputs/CrossTextInput'
-import ListInput from './inputs/ListInput'
-import RadioInput from './inputs/RadioInput'
-import SwitchInput from './inputs/SwitchInput'
+import { default as ProgressFab } from './buttons/ProgressFab/ProgressFab'
+import { default as TextInput } from './inputs/TextInput'
+import { default as ColorsInput } from './inputs/ColorsInput'
+import { default as CrossTextInput } from './inputs/CrossTextInput'
+import { default as ListInput } from './inputs/ListInput'
+import { default as RadioInput } from './inputs/RadioInput'
+import { default as SwitchInput } from './inputs/SwitchInput'
 
 export { ProgressFab, TextInput, ColorsInput, CrossTextInput, ListInput, RadioInput, SwitchInput }
